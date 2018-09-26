19820bdc177e2865e3efa290e8979e9ee132dabb
Mariano Benzo <mariano.benzo@despegar.com>
2018-09-26 00:30:56 -0300

    Update ConcurrentHashMap.cpp
    Hice un par de funciones, no las pude probar porque no lee el archivo

--- TP1/TP1-pthreads/entregable/ConcurrentHashMap.cpp
+++ TP1/TP1-pthreads/entregable/ConcurrentHashMap.cpp
@@ -1,24 +1,44 @@
+#include <utility>
+
+#include <utility>
+
 #ifndef CHM_CPP
 #define CHM_CPP
 
 #include <algorithm>
 #include <atomic>
-#include <ctype.h>
+#include <cctype>
 #include <utility>
 #include <fstream>
 #include <list>
+#include <vector>
 #include <string>
 #include <pthread.h>
+#include <fstream>
+#include <sstream>
 #include "ConcurrentHashMap.hpp"
 
 using namespace std;
 
 ConcurrentHashMap::ConcurrentHashMap() {
-    // Completar
+    for (auto &i : tabla) {
+        i = new Lista<pair<string, unsigned int>>();
+    }
 }
 
 void ConcurrentHashMap::addAndInc(string key) {
-    // Completar
+
+    int index = hash_key(key);
+
+    if (value(key) == 0) {
+        pair<string, unsigned int> p(key, 1);
+        tabla[index]->push_front(p);
+    } else {
+        for (auto it = tabla[index]->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
+            auto &t = it.Siguiente();
+            t.second++;
+        }
+    }
 }
 
 list<string> ConcurrentHashMap::keys() {
@@ -26,15 +46,47 @@ list<string> ConcurrentHashMap::keys() {
 }
 
 unsigned int ConcurrentHashMap::value(string key) {
-    // Completar
+
+    int index = hash_key(key);
+    unsigned int value = 0;
+    Lista<pair<string, unsigned int>> *tablaValue = tabla[index];
+
+    for (auto it = tablaValue->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
+        auto t = it.Siguiente();
+        if (t.first == key) {
+            value = t.second;
+        }
+    }
+
+    return value;
+
 }
 
 pair<string, unsigned int> ConcurrentHashMap::maximum(unsigned int n) {
     // Completar
 }
 
+int ConcurrentHashMap::hash_key(string key) {
+    return key.at(0) - 97;
+}
+
 static ConcurrentHashMap countWordsInFile(string filePath) {
-    // Completar
+
+    auto map = ConcurrentHashMap();
+    string line;
+    ifstream file;
+    file.open(filePath);
+    if (file.is_open()){
+        string word;
+        while (file >> word)
+        {
+            map.addAndInc(word);
+        }
+    }else {
+        perror("Error al abrir el archivo: ");
+    }
+    file.close();
+    return map;
 }
 
 static ConcurrentHashMap countWordsOneThreadPerFile(list <string> filePaths) {
