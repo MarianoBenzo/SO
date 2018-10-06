9fdddb3b5db665140991d87eaeec2bd82d3c5055
chino117 <chino117@hotmail.com>
2018-10-05 22:07:09 -0300

    Create test-tiempos.cpp

---
+++ TP1/TP1-pthreads/entregable/test-tiempos.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <pthread.h>
+#include "ConcurrentHashMap.cpp"
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char** argv){
+
+    if (argc < 2)
+        cout<<"LA CAGASTE"<<endl;
+
+    int cantThreads = atoi(argv[1]);
+    int cantArchivos = atoi(argv[2]);
+
+    list<string> archivos;
+    for (int i = 0; i < cantArchivos; i++){
+        string path = "casos_tests_corpus/corpus_";
+        path += to_string(i);
+        archivos.push_back(path);
+    }
+
+    cout<< cantThreads<<", "<<cantArchivos<<", ";
+    pair<string, unsigned int> maximo = maximumOne(cantThreads, 6, archivos);
+    cout<<", ";
+    pair<string, unsigned int> maximo_ = maximumTwo(cantThreads, 6, archivos);
+    cout<<"\n";
+
+    return 0;
+}
