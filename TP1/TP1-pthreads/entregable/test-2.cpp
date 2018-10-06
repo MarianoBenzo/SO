0f4c7c20b9fa0ce9e9b838cdcdfb57ae83d277d0
brianarrondo <brian_94_a@hotmail.com>
2018-10-05 23:16:31 -0300

    Update test-2.cpp

--- TP1/TP1-pthreads/entregable/test-2.cpp
+++ TP1/TP1-pthreads/entregable/test-2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int Test::run(int argc, char **argv) {
     ConcurrentHashMap h;
-    list <string> l = {"texto.txt", "texto-2.txt", "texto-3.txt"};
+    list <string> l = {"corpus-0", "corpus-1", "corpus-2"};
     int i;
 
     if (argc != 2) {
