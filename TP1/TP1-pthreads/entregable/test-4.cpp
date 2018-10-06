c11f8d39f0043b3914b04e1fbd18e1cf6424a76a
chino117 <chino117@hotmail.com>
2018-10-05 21:50:43 -0300

    Create test-4.cpp

---
+++ TP1/TP1-pthreads/entregable/test-4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "ConcurrentHashMap.cpp"
+
+using namespace std;
+
+int main(void) {
+	int i;
+	
+	ConcurrentHashMap h(countWordsInFile("corpus"));
+	for (i = 0; i < 26; i++) {
+		for (auto it = h.tabla[i]->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
+			auto t = it.Siguiente();
+			cout << t.first << " " << t.second << endl;
+		}
+	}
+
+	return 0;
+}
+
